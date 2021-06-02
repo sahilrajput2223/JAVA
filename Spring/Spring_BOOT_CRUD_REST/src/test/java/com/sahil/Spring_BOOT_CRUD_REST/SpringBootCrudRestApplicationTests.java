9c62d3a934c0a9a73e521f62226a4d4cf6b79b75
Sahil Rajput <rajputsahil.2204@gmail.com>
2021-06-03 00:17:37 +0530

    Spring BOOT CRUD REST(BASIC) API Code Added

---
+++ Spring/Spring_BOOT_CRUD_REST/src/test/java/com/sahil/Spring_BOOT_CRUD_REST/SpringBootCrudRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahil.Spring_BOOT_CRUD_REST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
